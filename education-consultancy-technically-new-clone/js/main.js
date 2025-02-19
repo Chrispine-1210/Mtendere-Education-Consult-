d2c47781772e5a725299840da7702810c2439cd9
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-02-19 17:23:08 +0200

    JS portfolio isotope and filter including testmoniial

--- education-consultancy-technically-new-clone/js/main.js
+++ education-consultancy-technically-new-clone/js/main.js
@@ -131,6 +131,72 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
+  // Portfolio isotope and filter
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
+  $('#portfolio-flters li').on('click', function () {
+    $("#portfolio-flters li").removeClass('active');
+    $(this).addClass('active');
+
+    portfolioIsotope.isotope({filter: $(this).data('filter')});
+  });
+
+
+  // Team carousel
+  $(".team-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+  });
+
 // custom code
 
 });
