93f77f11ad9fa9e52cf6bfbca6fbdaa3459036dd
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-03-03 22:14:13 +0200

    NEW CHANGES

--- mtendere-education-consult/styles/config/config/server.js
+++ mtendere-education-consult/styles/config/config/server.js
@@ -1,13 +1,23 @@
 const { connection } = require('next/server');
 
 require('dotenv').config();
-cost express = require('express');
-cost cors = require('cors');
-cost connectDB = require('.config/db');
-cost app = express();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('.config/db');
+const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
-app.get('/' (req, res) => res.send('API running...'));
-cost PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('server running on port ${PORT' ());
+app.get('/', (req, res) => res.send('API running...'));//+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+// Next.js Server Middleware);
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
