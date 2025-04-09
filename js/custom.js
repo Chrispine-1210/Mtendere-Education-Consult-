55f07764744070c561c42e54d333574953ecb5e1
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-04-09 04:26:06 +0200

    New changes

--- js/custom.js
+++ js/custom.js
@@ -296,6 +296,19 @@ $(function () {
 		});
 	});
 
+	// Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$(".back-to-top").fadeIn("slow");
+		} else {
+			$(".back-to-top").fadeOut("slow");
+		}
+	});
+	$(".back-to-top").click(function () {
+		$("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+		return false;
+	});
+
 
 	$('button').click(function () {
 		$('.pop-up-content').addClass('open');
