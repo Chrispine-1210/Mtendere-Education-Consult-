25bf1e17650edd1b7cd66a32c20dfd6d6e0bcb73
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-05-10 07:49:59 -0700

    New Changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,71 +4,17 @@ AOS.init({
 	once: true
 });
 
+// Initialize WOW.js
+new WOW().init();
+// Initialize Bootstrap tooltips
 $(function () {
+	$('[data-toggle="tooltip"]').tooltip();
 
-	'use strict';
-
-	$(".loader").delay(200).fadeOut("slow");
-	$("#overlayer").delay(200).fadeOut("slow");
-
-	var siteMenuClone = function () {
-
-		$('.js-clone-nav').each(function () {
-			var $this = $(this);
-			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
-		});
-
-		$('body').toggleClass('no-scroll', $('body').hasClass('offcanvas-menu'));
-
-		$('body').on('click', '.arrow-collapse', function (e) {
-			var $this = $(this);
-			if ($this.closest('li').find('.collapse').hasClass('show')) {
-				$this.removeClass('active');
-			} else {
-				$this.addClass('active');
-			}
-			e.preventDefault();
-
-		});
-
-		$(window).resize(function () {
-			var $this = $(this),
-				w = $this.width();
-
-			if (w > 768) {
-				if ($('body').hasClass('offcanvas-menu')) {
-					$('body').removeClass('offcanvas-menu');
-				}
-			}
-		})
-
-		$('body').on('click', '.js-menu-toggle', function (e) {
-			var $this = $(this);
-			e.preventDefault();
-
-			if ($('body').hasClass('offcanvas-menu')) {
-				$('body').removeClass('offcanvas-menu');
-				$('body').find('.js-menu-toggle').removeClass('active');
-			} else {
-				$('body').addClass('offcanvas-menu');
-				$('body').find('.js-menu-toggle').addClass('active');
-			}
-		})
-
-		// click outisde offcanvas
-		$(document).mouseup(function (e) {
-			var container = $(".site-mobile-menu");
-			if (!container.is(e.target) && container.has(e.target).length === 0) {
-				if ($('body').hasClass('offcanvas-menu')) {
-					$('body').removeClass('offcanvas-menu');
-					$('body').find('.js-menu-toggle').removeClass('active');
-				}
-			}
-		});
-	};
-	siteMenuClone();
+	$('[data-toggle="popover"]').popover();
+	$('.popover-dismiss').popover({
+		trigger: 'focus'
+	});
 });
-
 var owlPlugin = function () {
 	if ($('.owl-3-slider').length > 0) {
 		var owl3 = $('.owl-3-slider').owlCarousel({
