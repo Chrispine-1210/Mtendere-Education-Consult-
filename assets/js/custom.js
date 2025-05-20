ef5d47ede5d8b63f4617f4d59877359bb1ad08a8
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-05-19 23:45:09 -0700

    New Changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -274,18 +274,18 @@ var x = setInterval(function () {
 
 })
 
-$(document).ready(function(){
-  $('.courses-carousel').owlCarousel({
-    loop: true,
-    margin: 20,
-    nav: true,
-    dots: true,
-    responsive:{
-      0:{ items:1 },
-      600:{ items:2 },
-      1000:{ items:3 }
-    }
-  });
+$(document).ready(function () {
+    $('.courses-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: true,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3 }
+        }
+    });
 });
 
 const showToast = (message) => {
